09f6dd245700eb5982abf7c31f359f94c726ae8e
wcw5 <wcw5@illinois.edu>
2020-11-06 18:45:10 -0600

    dsyfunctional server running webaudio example

--- api/app.py
+++ api/app.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, url_for, render_template
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
 @app.route('/')
-def hello():
-    return 'Hello, world!'
+def index():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    socketio.run(app)
