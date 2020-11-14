9db75a0b8a86db92add5a9aef81cb0ca09d1b063
Jake Elder <jaelder2@illinois.edu>
2020-11-13 18:48:42 -0600

    Added basic slider along with Hello World

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import RangeSlider from 'react-bootstrap-range-slider';
+import 'react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css';
+
+const MyComponent = () => {
+
+    const [ value, setValue ] = useState(0);
+  
+    return (
+      <RangeSlider
+        value={value}
+        onChange={changeEvent => setValue(changeEvent.target.value)}
+      />
+    );
+  
+};
 
 var element = React.createElement('h1', { className: 'greeting' }, 'Hello, world!');
-ReactDOM.render(element, document.getElementById('root'));
+
+const elements = [<RangeSlider />, element];
+
+ReactDOM.render(elements, document.getElementById('root'));
